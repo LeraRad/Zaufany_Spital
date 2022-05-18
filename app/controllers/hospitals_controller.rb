1625b864c209055d83f7d73c819aa97068330e8d
LeraRad <exlab7@gmail.com>
2022-05-18 13:40:32 +0200

    Added create and new actions to the Hospital controller

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -2,4 +2,23 @@ class HospitalsController < ApplicationController
   def index
     @hospital_items = Hospital.all
   end
+
+  def new
+    @hospital_item = Hospital.new
+  end
+
+  def create
+    @hospital_item = Hospital.new(params.require(:hospital).permit(:title,:hospital_email,:hospital_phone))
+
+    respond_to do |format|
+      if @hospital_item.save
+        format.html { redirect_to hospitals_path, notice: "New Hospital was successfully created." }
+        format.json { render :show, status: :created, location: @hospital }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @hospital.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 end
